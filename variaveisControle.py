bda603dd7cfc1be49286a7389de4c00ab789d2cf
Tiago Teixeira Prazeres <tiago_teixeira_prazeres@yahoo.com.br>
2021-07-23 20:59:28 -0300

    Sistema com Login-Cliente-Produto

---
+++ variaveisControle.py
@@ -0,0 +1,13 @@
+################ VARIÁVEIS formDadosCliente ################
+tipoTelaDadosCliente = '' # incluir, alterar e consultar
+idConsulta = ''
+
+################ VARIÁVEIS formDadosProduto ################
+tipoTelaDadosProduto = '' # incluir, alterar e consultar
+idConsulta = ''
+
+################ CONEXÃO COM BANCO DE DADOS ################
+host = 'localhost'
+user = 'tiago'
+password = '1234'
+database = 'python'
